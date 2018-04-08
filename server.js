66a8f68e40117b2ab77620704c3af3d3c1f0a28e
David <st149799@stud.uni-stuttgart.de>
2018-04-08 13:11:47 +0200

    html läuft

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+const azure = require('azure');
+var fs = require("fs");
 var http = require('http');
+
+var html = fs.readFileSync("HTMLPage.html").toString();
+html = html.replace("<style></style>","<style>"+fs.readFileSync("style.css")+"</style>");
+var output = "<p>none</p>";
 
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+    res.end(html.replace("<output>",output));
     
 }).listen(process.env.PORT || 8080);
